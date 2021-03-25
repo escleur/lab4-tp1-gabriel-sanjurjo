664f6ef2901feecce8dae992052fd8900aa2265c
escleur <escleur@gmail.com>
2021-03-25 18:12:18 -0300

    Componentes home, quien soy y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './componente/login/login.component';
+import { HomeComponent } from './componente/home/home.component';
+import { QuienSoyComponent } from './componente/quien-soy/quien-soy.component';
+
+//Angular Material
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
